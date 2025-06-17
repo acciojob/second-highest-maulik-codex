aea37df2063f6ae5637cf9585758f9fc6c2c9266
Maulik Chhansiya <maulikchhansiya@gmail.com>
2025-06-17 22:17:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity;
+
+  let max = -Infinity;
+  let second = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+
+    if (num > max) {
+      second = max;
+      max = num;
+    } else if (num > second && num < max) {
+      second = num;
+    }
+  }
+
+  // If all elements are same, second will remain -Infinity
+  return second;
+}
+
